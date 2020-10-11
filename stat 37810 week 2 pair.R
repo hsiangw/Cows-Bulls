26cacd143021236227babaebaf0393c41bc3cb3b
hsiangw <hsiangw@uchicago.edu>
2020-10-11 15:33:50 -0500

    Problem 2

--- stat 37810 week 2 pair.R
+++ stat 37810 week 2 pair.R
@@ -1,20 +1,22 @@
 bulls_and_cows <- function(){
   generate_computer_vector <- function(){
+    #This function generates computer choice
     computer_choice <- sample(0:9,4)
     return(computer_choice)
   }
   get_guess <- function(remaining){
+    #This function inputs number of remaining choice and outputs user choice
     print(paste("Number of remaining guesses:",remaining))
-    chance <- 3
+    chance <- 3 #Allowing three errors
     while(chance>0){
       chance <- chance-1
       numbers_string <- readline("Please enter four numbers > ")#input user's guess 
       user_choice<-as.integer(unlist(strsplit(numbers_string, ""))) # as numeric vector
-      if (length(user_choice)!=4){
+      if (length(user_choice)!=4){ #If the unit of inputs is not 4
         message("The length of user inputs are not correct")
         next
       }
-      if(any(duplicated(user_choice))){
+      if(any(duplicated(user_choice))){ #If there is duplicated
         message("There are duplicated numbers in user inputs")
         next
       }
@@ -23,7 +25,8 @@ bulls_and_cows <- function(){
     
   }
   number_bulls_and_cows <- function(computer_choice,user_choice){
-    nbulls <- function(computer_choice,user_choice){
+    #This function inputs computer and user choice. It outputs number of bulls and cows
+    nbulls <- function(computer_choice,user_choice){ #compute number of bulls
       bull <- 0
       for (i in 1:length(computer_choice)){
         if (computer_choice[i]==user_choice[i]){
@@ -32,14 +35,14 @@ bulls_and_cows <- function(){
       }
       bull
     }
-    ncows <- function(computer_choice,user_choice){
+    ncows <- function(computer_choice,user_choice){#compute numbers of cows
       length(intersect(computer_choice,user_choice))-nbulls(computer_choice,user_choice)
     }
     nbull <- nbulls(computer_choice,user_choice)
     ncow <- ncows(computer_choice,user_choice)
     return(c(nbull,ncow))
   }
-  do_response <- function(computer_choice){
+  do_response <- function(computer_choice){ #return the correct value
     vectoint <- function(vec){
       value=0
       for (i in 1:length(vec)){
